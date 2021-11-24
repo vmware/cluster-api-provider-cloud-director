514d19c5a2b2aea1d5cb44bae52aef6b4a8094bb
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2021-11-23 17:21:45 -0800

    Change capvcd cluster schema
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- controllers/vcdcluster_controller.go
+++ controllers/vcdcluster_controller.go
@@ -33,7 +33,7 @@ import (
 
 const (
 	CAPVCDTypeVendor  = "vmware"
-	CAPVCDTypeNss     = "capvcd"
+	CAPVCDTypeNss     = "capvcdcluster"
 	CAPVCDTypeVersion = "1.0.0"
 
 	CAPVCDClusterKind             = "CAPVCDCluster"
