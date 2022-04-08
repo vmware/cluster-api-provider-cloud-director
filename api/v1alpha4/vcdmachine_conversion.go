542a72df573f3438fd3c53a767affc85b6979de0
sayloo <sahithi.ayloo@gmail.com>
2022-04-08 12:49:23 -0700

    Addressed review comments
    Signed-off-by: sayloo <sahithi.ayloo@gmail.com>

--- api/v1alpha4/vcdmachine_conversion.go
+++ api/v1alpha4/vcdmachine_conversion.go
@@ -5,7 +5,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
-// ConvertTo converts this (v1alpha4)VCDCluster to the Hub version (v1alpha5).
+// ConvertTo converts this (v1alpha4)VCDCluster to the Hub version (v1beta1).
 func (src *VCDMachine) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta1.VCDMachine)
 
@@ -16,7 +16,7 @@ func (src *VCDMachine) ConvertTo(dstRaw conversion.Hub) error {
 	return nil
 }
 
-// ConvertFrom converts from the Hub version (v1alpha5) to this version (v1alpha4).
+// ConvertFrom converts from the Hub version (v1beta1) to this version (v1alpha4).
 func (dst *VCDMachine) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1beta1.VCDMachine)
 	if err := Convert_v1beta1_VCDMachine_To_v1alpha4_VCDMachine(src, dst, nil); err != nil {
