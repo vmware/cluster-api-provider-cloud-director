6f041a9a542b6a061c7bcb4dc0305f954c9f8e69
ymo24 <102765549+ymo24@users.noreply.github.com>
2022-04-20 15:32:18 -0700

    [VCDA-3532] Add RDEId into VApp metadata (#103)
    * add metadata into VApp
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * reduce duplicated patchVCDCluster, avoid potential mix
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * Status field vAppMetadataUpdated should be optional
    
    Signed-off-by: ymo24 <ymo@vmware.com>

--- api/v1alpha4/conversion.go
+++ api/v1alpha4/conversion.go
@@ -21,3 +21,7 @@ func Convert_v1alpha4_VCDMachineSpec_To_v1beta1_VCDMachineSpec(in *VCDMachineSpe
 func Convert_v1beta1_VCDMachineSpec_To_v1alpha4_VCDMachineSpec(in *v1beta1.VCDMachineSpec, out *VCDMachineSpec, s conversion.Scope) error {
 	return autoConvert_v1beta1_VCDMachineSpec_To_v1alpha4_VCDMachineSpec(in, out, s)
 }
+func Convert_v1beta1_VCDClusterStatus_To_v1alpha4_VCDClusterStatus(in *v1beta1.VCDClusterStatus, out *VCDClusterStatus, s conversion.Scope) error {
+	// Todo: check if VCDClusterStatus.vAppMetadata_Updated needs to be updated (VCDA-3532)
+	return autoConvert_v1beta1_VCDClusterStatus_To_v1alpha4_VCDClusterStatus(in, out, s)
+}
