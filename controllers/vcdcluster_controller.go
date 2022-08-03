befab0a48bd8ca8b1dde7c92e3f89dfc137c4638
Anirudh9794 <aniruddha.9794@gmail.com>
2022-08-03 13:55:15 -0700

    Add check for no rde before getting the RDE entity type version (#221)
    * Add check for no rde before getting the RDE entity type version
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address review comment and add RdeVersionInUse as No_RDE_ for NO_RDE clusters
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- controllers/vcdcluster_controller.go
+++ controllers/vcdcluster_controller.go
@@ -537,13 +537,15 @@ func (r *VCDClusterReconciler) reconcileNormal(ctx context.Context, cluster *clu
 	// TODO validate if the RDE ID format is correct.
 	if infraID == "" && len(vcdCluster.Spec.RDEId) > 0 {
 		infraID = vcdCluster.Spec.RDEId
-		_, rdeVersion, err := capvcdRdeManager.GetRDEVersion(ctx, infraID)
-		if err != nil {
-			return ctrl.Result{}, errors.Wrapf(err,
-				"\"Unexpected error retrieving RDE [%s] for the cluster [%s]", infraID, vcdCluster.Name)
+		if !strings.HasPrefix(vcdCluster.Spec.RDEId, NoRdePrefix) {
+			_, rdeVersion, err := capvcdRdeManager.GetRDEVersion(ctx, infraID)
+			if err != nil {
+				return ctrl.Result{}, errors.Wrapf(err,
+					"\"Unexpected error retrieving RDE [%s] for the cluster [%s]", infraID, vcdCluster.Name)
+			}
+			// update the RdeVersionInUse with the entity type version of the rde.
+			vcdCluster.Status.RdeVersionInUse = rdeVersion
 		}
-		// update the RdeVersionInUse with the entity type version of the rde.
-		vcdCluster.Status.RdeVersionInUse = rdeVersion
 	}
 
 	// Create a new RDE if it was not already created or assigned.
@@ -633,7 +635,11 @@ func (r *VCDClusterReconciler) reconcileNormal(ctx context.Context, cluster *clu
 		oldVCDCluster := vcdCluster.DeepCopy()
 
 		vcdCluster.Status.InfraId = infraID
-		vcdCluster.Status.RdeVersionInUse = rdeType.CapvcdRDETypeVersion
+		if strings.HasPrefix(infraID, NoRdePrefix) {
+			vcdCluster.Status.RdeVersionInUse = NoRdePrefix
+		} else {
+			vcdCluster.Status.RdeVersionInUse = rdeType.CapvcdRDETypeVersion
+		}
 		if err := r.Status().Patch(ctx, vcdCluster, client.MergeFrom(oldVCDCluster)); err != nil {
 			return ctrl.Result{}, errors.Wrapf(err,
 				"unable to patch status of vcdCluster [%s] with InfraID [%s], RDEVersion [%s]",
