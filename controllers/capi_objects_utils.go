6514a4a8267c37687c4ff67dbfad843291af9c87
Anirudh9794 <aniruddha.9794@gmail.com>
2022-09-14 12:18:19 -0700

    [VCDA-4379] Add tkgVersion and kubernetes version as part of capvcd.upgrade in RDE (#253)
    * Add tkgVersion and kubernetes version as part of capvcd.upgrade in RDE
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address more review comments
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- controllers/capi_objects_utils.go
+++ controllers/capi_objects_utils.go
@@ -440,3 +440,11 @@ func getUserCredentialsForCluster(ctx context.Context, cli client.Client, define
 
 	return userCredentials, nil
 }
+
+// hasKcpReconciledToDesiredK8Version determines if the rolling upgrade process is complete. Returns false if the upgrade is still on going
+func hasKcpReconciledToDesiredK8Version(kcp *kcpv1.KubeadmControlPlane) bool {
+	return kcp != nil && kcp.Status.Ready &&
+		kcp.Status.Version != nil &&
+		*kcp.Status.Version == kcp.Spec.Version &&
+		kcp.Status.UnavailableReplicas == 0
+}
