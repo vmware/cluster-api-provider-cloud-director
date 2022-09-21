7aa9532391b04f6c1a855b99bf8a01a59f0deffe
Anirudh9794 <aniruddha.9794@gmail.com>
2022-09-21 15:47:00 -0700

    [VCDA-4430] Add available nodes to the node pools section in the RDE (#264)
    * Add available nodes in the node pools
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Add examples for the node pools
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * change replicas to desiredReplicas
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- pkg/vcdtypes/rde_type_1_1_0/types_1_1_0.go
+++ pkg/vcdtypes/rde_type_1_1_0/types_1_1_0.go
@@ -100,14 +100,15 @@ type VCDResource struct {
 }
 
 type NodePool struct {
-	Name            string            `json:"name,omitempty"`
-	SizingPolicy    string            `json:"sizingPolicy,omitempty"`
-	PlacementPolicy string            `json:"placementPolicy,omitempty"`
-	DiskSizeMb      int32             `json:"diskSizeMb,omitempty"`
-	NvidiaGpu       bool              `json:"nvidiaGpu,omitempty"`
-	StorageProfile  string            `json:"storageProfile,omitempty"`
-	Replicas        int32             `json:"replicas,omitempty"`
-	NodeStatus      map[string]string `json:"nodeStatus,omitempty"`
+	Name              string            `json:"name,omitempty"`
+	SizingPolicy      string            `json:"sizingPolicy,omitempty"`
+	PlacementPolicy   string            `json:"placementPolicy,omitempty"`
+	DiskSizeMb        int32             `json:"diskSizeMb,omitempty"`
+	NvidiaGpuEnabled  bool              `json:"nvidiaGpuEnabled,omitempty"`
+	StorageProfile    string            `json:"storageProfile,omitempty"`
+	DesiredReplicas   int32             `json:"desiredReplicas,omitempty"`
+	AvailableReplicas int32             `json:"availableReplicas,omitempty"`
+	NodeStatus        map[string]string `json:"nodeStatus,omitempty"`
 }
 
 type ClusterResourceSetBinding struct {
