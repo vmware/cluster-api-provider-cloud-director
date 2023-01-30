d81bce04ecfa54bc085aa7642dcc626ad1254b39
Erkan Erol <erkan@giantswarm.io>
2023-01-30 16:20:23 +0300

    Set computer name as vm name

--- go.mod
+++ go.mod
@@ -111,3 +111,5 @@ require (
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
+
+replace github.com/vmware/cloud-provider-for-cloud-director => github.com/giantswarm/cloud-provider-for-cloud-director v0.0.0-20230130130729-9e1cd82916be
--- vendor/github.com/vmware/cloud-provider-for-cloud-director/pkg/vcdsdk/vapp.go
+++ vendor/github.com/vmware/cloud-provider-for-cloud-director/pkg/vcdsdk/vapp.go
@@ -653,6 +653,7 @@ func (vdc *VdcManager) AddNewMultipleVM(vapp *govcd.VApp, vmNamePrefix string, v
 						AdminPasswordEnabled:  &trueVar,
 						AdminPasswordAuto:     &trueVar,
 						ResetPasswordRequired: &falseVar,
+						ComputerName:          vmName,
 					},
 					NetworkConnectionSection: &types.NetworkConnectionSection{
 						NetworkConnection: []*types.NetworkConnection{
