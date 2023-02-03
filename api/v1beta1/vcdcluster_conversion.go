8f0a943acd1f09800be5105a764482d4924b3e9d
Adnan Baruni <abaruni@outlook.com>
2023-02-03 11:01:36 -0600

    update conversion funcs

--- api/v1beta1/vcdcluster_conversion.go
+++ api/v1beta1/vcdcluster_conversion.go
@@ -2,19 +2,32 @@ package v1beta1
 
 import (
 	"github.com/vmware/cluster-api-provider-cloud-director/api/v1beta2"
+	utilconversion "sigs.k8s.io/cluster-api/util/conversion"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
 // ConvertTo converts this VCDCluster to the Hub version (v1beta2).
 func (src *VCDCluster) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta2.VCDCluster)
-	return Convert_v1beta1_VCDCluster_To_v1beta2_VCDCluster(src, dst, nil)
+	if err := Convert_v1beta1_VCDCluster_To_v1beta2_VCDCluster(src, dst, nil); err != nil {
+		return err
+	}
+
+	restored := v1beta2.VCDCluster{}
+	if ok, err := utilconversion.UnmarshalData(src, restored); err != nil || !ok {
+		return err
+	}
+	return nil
 }
 
 // ConvertFrom converts from the Hub version (v1beta2) to this version (v1beta1).
 func (dst *VCDCluster) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1beta2.VCDCluster)
-	return Convert_v1beta2_VCDCluster_To_v1beta1_VCDCluster(src, dst, nil)
+	if err := Convert_v1beta2_VCDCluster_To_v1beta1_VCDCluster(src, dst, nil); err != nil {
+		return err
+	}
+
+	return utilconversion.MarshalData(src, dst)
 }
 
 // ConvertTo converts this VCDClusterList to the Hub version (v1beta2).
--- api/v1beta1/vcdmachinetemplate_conversion.go
+++ api/v1beta1/vcdmachinetemplate_conversion.go
@@ -2,13 +2,23 @@ package v1beta1
 
 import (
 	"github.com/vmware/cluster-api-provider-cloud-director/api/v1beta2"
+	utilconversion "sigs.k8s.io/cluster-api/util/conversion"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
 // ConvertTo converts this VCDMachineTemplate to the Hub version (v1beta2).
 func (src *VCDMachineTemplate) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta2.VCDMachineTemplate)
-	return Convert_v1beta1_VCDMachineTemplate_To_v1beta2_VCDMachineTemplate(src, dst, nil)
+	if err := Convert_v1beta1_VCDMachineTemplate_To_v1beta2_VCDMachineTemplate(src, dst, nil); err != nil {
+		return err
+	}
+
+	restored := v1beta2.VCDMachineTemplate{}
+	if ok, err := utilconversion.UnmarshalData(src, restored); err != nil || !ok {
+		return err
+	}
+
+	return nil
 }
 
 // ConvertFrom converts from the Hub version (v1beta2) to this version (v1beta1).
