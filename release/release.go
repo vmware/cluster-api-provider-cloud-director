0f1364768b18b106b22f00c4c52267612b165a2c
Arun Krishnakumar <akrishnakuma@vmware.com>
2023-06-16 11:03:05 -0700

    CAFV-281: Use same version methodology as other components

--- release/release.go
+++
@@ -1,8 +0,0 @@
-package release
-
-import (
-	_ "embed" // this needs go 1.16+
-)
-
-//go:embed version
-var CAPVCDVersion string
---
+++ release/version.go
@@ -0,0 +1,9 @@
+/*
+   Copyright 2021 VMware, Inc.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package release
+
+// Version is to be set in the build process. Empty version will cause a panic.
+var Version string = ""
