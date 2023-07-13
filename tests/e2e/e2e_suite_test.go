a641a0b11a3d215543e51de235d572bf05ed8397
ymo24 <102765549+ymo24@users.noreply.github.com>
2023-07-12 17:18:14 -0700

    [CAFV-267] Clutser upgrade test automation (#475)
    * add files
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * add updates for cluster upgrade
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * add doc update
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * address comments
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * minor fix
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * add refrator jira task reminder
    
    Signed-off-by: ymo24 <ymo@vmware.com>

--- tests/e2e/e2e_suite_test.go
+++ tests/e2e/e2e_suite_test.go
@@ -8,13 +8,17 @@ import (
 )
 
 var (
-	PathToMngmntClusterKubecfg    string
-	PathToWorkloadClusterCapiYaml string
+	PathToMngmntClusterKubecfg     string
+	PathToWorkloadClusterCapiYaml  string
+	TargetTKGVersionForUpgradeTest string
+	TargetK8SVersionForUpgradeTest string
 )
 
 func init() {
 	flag.StringVar(&PathToMngmntClusterKubecfg, "PathToMngmntClusterKubecfg", "", "path to find the Kubeconfig. It is used to retrieve the cluster")
 	flag.StringVar(&PathToWorkloadClusterCapiYaml, "PathToWorkloadClusterCapiYaml", "", "path to find the capi Yaml. It is used to generate the vcd cluster")
+	flag.StringVar(&TargetTKGVersionForUpgradeTest, "TargetTKGVersionForUpgradeTest", "", "target TKG version for upgrade test")
+	flag.StringVar(&TargetK8SVersionForUpgradeTest, "TargetK8SVersionForUpgradeTest", "", "target Kubernetes version for upgrade test")
 }
 
 var _ = BeforeSuite(func() {
