6871b71724ec2e0baac11c18d7d5b97731d5a942
Adnan Baruni <abaruni@outlook.com>
2024-03-14 12:30:46 -0500

    fix conversion errors

--- api/v1beta1/conversion.go
+++ api/v1beta1/conversion.go
@@ -24,3 +24,7 @@ func Convert_v1beta1_VCDClusterSpec_To_v1beta3_VCDClusterSpec(in *VCDClusterSpec
 func Convert_v1beta3_VCDClusterStatus_To_v1beta1_VCDClusterStatus(in *v1beta3.VCDClusterStatus, out *VCDClusterStatus, s conversion.Scope) error {
 	return autoConvert_v1beta3_VCDClusterStatus_To_v1beta1_VCDClusterStatus(in, out, s)
 }
+
+func Convert_v1beta3_VCDMachineStatus_To_v1beta1_VCDMachineStatus(in *v1beta3.VCDMachineStatus, out *VCDMachineStatus, s conversion.Scope) error {
+	return autoConvert_v1beta3_VCDMachineStatus_To_v1beta1_VCDMachineStatus(in, out, s)
+}
